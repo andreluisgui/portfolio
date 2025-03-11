49b08d9311efca55aeb1e703c92ae75c1a7da442
andresoares <andresoares@sefaz.pi.gov.br>
2025-03-11 11:29:08 -0300

    ajustes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    unoptimized: true, // já desativa o uso do sharp para otimização de imagens
+  },
+  outputFileTracing: false, // desativa o rastreamento de arquivos
 };
 
 export default nextConfig;
